f6048e5b83036b1fe10a6b74bfca9d95eda4c317
Jan Pistor <pistor.jan@gmail.com>
2024-02-19 15:02:54 +0100

    refactor: try to use tanstack query in functional component

--- src/app/page.cljs
+++ src/app/page.cljs
@@ -1,12 +1,33 @@
 (ns app.page
   (:require [reagent.core :as r]
-            ["react-dom/client" :refer [createRoot]]))
+            ["react-dom/client" :refer [createRoot]]
+            [react :as react]
+            ["@tanstack/react-query" :as react-query]
+            [app.routes :as routes]))
 
 (defonce root (createRoot (js/document.getElementById "app")))
+(defonce query-client (new react-query/QueryClient))
 
+;; This is used with :f> so both Hooks and RAtoms work here
+(defn example []
+  (let [[count set-count] (react/useState 0)]
+        ;; [result (react-query/useQuery #js {:queryKey (into-array "todo")
+        ;;                                    :queryFn (fn [] (js/Promise.resolve #js [{:id 1 :title "Do the thing"}]))})]]
+
+    [:div
+     [:p "You clicked " count " times"]
+     [:button
+      {:on-click #(set-count inc)}
+      "Click"]]))
+
+;; (defn root []
+;;   [:div
+;;    [:f> example]])
 (defn app-view []
-  [:div
-   [:h1.text-2xl.text-amber-500 "Tailwind heading"]])
+  [:> react-query/QueryClientProvider {:client query-client}
+   [:div
+    [:h1.text-2xl.text-amber-500 "Tailwind heading"]]
+   [:f> example]])
 
 (defn ^:dev/after-load render []
   (.render root (r/as-element [app-view])))
---
+++ src/app/routes.cljs
@@ -0,0 +1,5 @@
+(ns app.routes)
+
+(defn main []
+  ;; (import.meta.globEager "/src/app/**/[a-z[]*.tsx")
+  (js/console.debug "Hello from routes.cljs"))
