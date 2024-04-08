bd4dfc3dc607834a0cda28959424138c7b8f68ce
Jan Pistor <pistor.jan@gmail.com>
2024-04-08 17:28:17 +0200

    working

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import * as i0 from "react";
-import * as i1 from "react-dom/client";
-import * as i2 from "@tanstack/react-query";
-import * as i3 from "@@/button";
-import * as i4 from "@@/calendar";
+import * as i0 from "react-refresh/runtime";
+import * as i1 from "react";
+import * as i2 from "react-dom/client";
+import * as i3 from "react-dom";
+import * as i4 from "@@/button";
+import * as i5 from "@@/calendar";
 
 const ALL = {};
 
@@ -15,12 +16,14 @@ globalThis.shadow$bridge = function(name) {
   }
 };
 
-ALL["react"] = i0;
+ALL["react-refresh/runtime"] = i0;
 
-ALL["react-dom/client"] = i1;
+ALL["react"] = i1;
 
-ALL["@tanstack/react-query"] = i2;
+ALL["react-dom/client"] = i2;
 
-ALL["@@/button"] = i3;
+ALL["react-dom"] = i3;
 
-ALL["@@/calendar"] = i4;
+ALL["@@/button"] = i4;
+
+ALL["@@/calendar"] = i5;
--- src/client/main.cljs
+++ src/client/main.cljs
@@ -1,41 +1,20 @@
 (ns client.main
-  (:require [reagent.core :as r]
-            ["react-dom/client" :refer [createRoot]]
-            [react :as react]
-            ["@tanstack/react-query" :as react-query]
-            ;; Require UI Components 
-            ["@@/button" :refer [Button]]
-            ["@@/calendar" :refer [Calendar]]))
-            ;; [app.routes :as routes]))
+  (:require
+   [uix.core :as uix :refer [defui $]]
+   [uix.dom]
+   ["@@/button" :refer [Button]]
+   ["@@/calendar" :refer [Calendar]]))
 
-(defonce root (createRoot (js/document.getElementById "app")))
-(defonce query-client (new react-query/QueryClient))
+(defui app []
+  ($ :div
+     ($ Button {:on-click #(js/alert "You Clicked")} "Click Me")
+     ($ Calendar)))
 
-;; This is used with :f> so both Hooks and RAtoms work here
-(defn example []
-  (let [[count set-count] (react/useState 0)]
-        ;; [result (react-query/useQuery #js {:queryKey (into-array "todo")
-        ;;                                    :queryFn (fn [] (js/Promise.resolve #js [{:id 1 :title "Do the thing"}]))})]]
+(defonce root
+  (uix.dom/create-root (js/document.getElementById "app")))
 
-    [:div
-     [:p "You clicked " count " times"]
-     [:button
-      {:on-click #(set-count inc)}
-      "Click"]]))
-
-(defn app-view []
-  [:> react-query/QueryClientProvider {:client query-client}
-   [:div
-    [:h1.text-2xl.text-amber-500 "Tailwind heading"]
-    [:> Button {:on-click #(js/alert "Hello")} "Hello Button"]
-    [:> Button {:on-click #(js/alert "Hello")} "Hello Button"]
-    [:> Calendar :mode "single"]
-
-    [:> Calendar :mode "single"]]
-   [:f> example]])
-
-(defn ^:dev/after-load render []
-  (.render root (r/as-element [app-view])))
+(defn render []
+  (uix.dom/render-root ($ app) root))
 
 (defn ^:export init []
   (render))
