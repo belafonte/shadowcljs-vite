2e19c96bd68d0ab3a8da8d50a8c626032e48174a
Jan Pistor <pistor.jan@gmail.com>
2024-06-03 09:51:32 +0200

    fix: only watch compiled cljs

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,28 @@
-import { defineConfig } from 'vite'
-import { fileURLToPath } from 'node:url'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { fileURLToPath } from "node:url";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      // Exclude .cljs files
+      // so changes dont trigger multiple reloads
+      ignored: "**/*.cljs",
+    },
+  },
   resolve: {
     alias: [
       {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src/client', import.meta.url))
+        find: "@",
+        replacement: fileURLToPath(new URL("./src/client", import.meta.url)),
       },
       {
-        find: '@@',
-        replacement: fileURLToPath(new URL('./src/client/components/ui', import.meta.url))
+        find: "@@",
+        replacement: fileURLToPath(
+          new URL("./src/client/components/ui", import.meta.url),
+        ),
       },
-    ]
-  }
+    ],
+  },
 });
